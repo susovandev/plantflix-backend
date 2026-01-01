d47b7503c95a710c1e3ebd67eaa023ddabcab8a7
Susovan Dev <susovandas.dev@gmail.com>
2026-01-01 11:51:57 +0530

    refactor(core): reorganize and clean up base code structure

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
+import { API_PREFIX } from '../constants';
 import { Application } from 'express';
 import authRouter from 'modules/auth/routes/auth.routes';
 import { healthCheckHandler } from 'modules/health/healthCheck.controller';
 export default function configureRoutes(app: Application) {
 	app.use('/health', healthCheckHandler);
-	app.use('/api/v1/auth', authRouter);
+	app.use(`${API_PREFIX}/auth`, authRouter);
 }
