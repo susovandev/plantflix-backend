228834843d2c233bc530cb286358192866904b0b
Susovan Dev <susovandas.dev@gmail.com>
2026-01-03 16:45:20 +0530

    refactor(auth): add more config option for cleanup job

--- src/jobs/queues/cleanup.queue.ts
+++ src/jobs/queues/cleanup.queue.ts
@@ -4,4 +4,13 @@ import { CLEANUP_QUEUE_NAME } from 'constants/Jobs/job.constants';
 
 export const cleanupQueue = new Queue(CLEANUP_QUEUE_NAME, {
 	connection: redisConnection,
+	defaultJobOptions: {
+		removeOnComplete: true,
+		removeOnFail: 50,
+		attempts: 3,
+		backoff: {
+			type: 'exponential',
+			delay: 5000,
+		},
+	},
 });
